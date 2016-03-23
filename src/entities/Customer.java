fee4d92543ef5c80e2ed6c394b964f604021da5a
ezj5050 <ezj5050@208-NWD33.up.ist.local>
2016-03-23 15:25:52 -0400

    Add variables on customer class and add new class "AshleyJeong"

--- src/entities/Customer.java
+++ src/entities/Customer.java
@@ -8,6 +8,11 @@ import java.awt.Rectangle;
  * @author jrimland
  */
 public abstract class Customer extends Rectangle {
+    
+    String name ;
+    int age;
+    String major;
+    
     public Customer()
     {
         
